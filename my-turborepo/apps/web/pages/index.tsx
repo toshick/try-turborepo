1b0d7c18792ff658baed960348e577577833b82f
toshick <toshickjazz@mac.com>
2022-01-14 18:30:45 +0900

    feat: try import from packages and apps

--- my-turborepo/apps/web/pages/index.tsx
+++ my-turborepo/apps/web/pages/index.tsx
@@ -1,10 +1,15 @@
-import { Button } from "ui";
+import { Button } from 'ui';
+import { DEBUG } from 'docs/debug';
 
 export default function Web() {
   return (
     <div>
       <h1>Web</h1>
       <Button />
+      <div>
+        docsディレクトリからのinport:
+        {DEBUG}
+      </div>
     </div>
   );
 }
